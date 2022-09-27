cef94d452433be42f5883b0175590b09d066bf16
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-27 08:45:15 -0300

    Segundo commit - Resolução, correção de provas.

--- 01/index.js
+++ 01/index.js
@@ -23,6 +23,27 @@ const prova = {
             resposta: "c",
             correta: "c"
         }
-    ]
+    ],
+    correcao: function () {
+        let acertos = 0;;
+        for (let acerto of prova.questoes) {
+            if (acerto.resposta === acerto.correta) {
+                acertos++;
+            }
+        }
+        const nota = acertos * 2;
+        console.log(`O aluno(a) ${prova.aluno} acertou ${acertos} questões e obiteve nota ${nota}.`)
+    }
 };
 
+// function corrigirProva(prova) {
+//     let acertos = 0
+//     for (let acerto of prova.questoes) {
+//         if (acerto.resposta === acerto.correta) {
+//             acertos++
+//         }
+//     }
+//     return acertos;
+
+// }
+prova.correcao();
