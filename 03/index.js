1b044f179dd2b1f28a55ba2f4cf446e54abb0860
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-30 08:22:28 -0300

    Resolução, etapa (C).

--- 03/index.js
+++ 03/index.js
@@ -13,37 +13,18 @@ const carrinho = {
             qtd: 2,
             precoUnit: 5000
         }
-    ]
-}
-function imprimirResumoDoCarinho(carrinho) {
-    console.log(`cliente: ${carrinho.nomeDoCliente}`);
-    let totalItens = 0;
-    let totalApagar = 0;
-    for (let busca of carrinho.produtos) {
-        totalItens = totalItens + busca.qtd;
-        totalApagar = totalApagar + (busca.precoUnit * busca.qtd);
+    ],
+    imprimirResumo: function () {
+        console.log(`cliente: ${this.nomeDoCliente}`);
+        let totalItens = 0;
+        let totalApagar = 0;
+        for (let busca of this.produtos) {
+            totalItens = totalItens + busca.qtd;
+            totalApagar = totalApagar + (busca.precoUnit * busca.qtd);
+        }
+        console.log(`Total de Itens: ${totalItens}`);
+        console.log(`Total a Pagar: R$${(totalApagar / 100).toFixed(2)}`)
+        // toLocaleString('pt-BR', { minimumFractionDigits: 2 })
     }
-    console.log(`Total de Itens: ${totalItens}`);
-    console.log(`Total a Pagar: R$${(totalApagar / 100).toFixed(2)}`)
-    // toLocaleString('pt-BR', { minimumFractionDigits: 2 })
 }
-
-
-
-
-// for (let busca of carrinho.produtos) {
-//     const { nomeDoCliente, produtos } = busca;
-//     let totalItens = 0;
-//     //if (busca.produtos > 0) {
-//     totalItens = [produtos].reduce(function (acumulador, valorAtual) {
-//         return acumulador + valorAtual;
-
-//     }, totalItens);
-//     console.log(totalItens);
-
-//     //}
-
-
-
-
 imprimirResumoDoCarinho(carrinho);
